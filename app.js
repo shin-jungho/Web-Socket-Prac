bc1648404bc5765a242536ea0786678bbaee04a1
jungho shin <wjdghtls11@gmail.com>
2022-02-12 18:04:13 +0900

    Fix: 몽고디비 연결에러 해결

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-const colorHash = require('color-hash');
+const ColorHash = require('color-hash');
 
 dotenv.config();
-const connect = require('./schemas');
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
 app.set('port', process.env.PORT || 8005);
@@ -19,15 +19,9 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+connect();
 
-connect(); //몽고디비 연결
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -35,11 +29,17 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+});
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
 
 app.use((req, res, next) => {
-  if(!req.session.color) {
-    const colorHash = new colorHash();
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
     req.session.color = colorHash.hex(req.sessionID);
   }
   next();
@@ -64,4 +64,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleware);
--- schemas/index.js
+++ schemas/index.js
@@ -4,13 +4,11 @@ const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = process.env;
 const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
 
 const connect = () => {
-  if (NODE_ENV !== 'production') {
+  if (NODE_ENV !== 'production') { // 
     mongoose.set('debug', true);
   }
   mongoose.connect(MONGO_URL, {
-    dbName: 'gifchat',
-    useNewUrlParser: true,
-    useCreateIndex: true,
+    dbName: 'gifchat'
   }, (error) => {
     if (error) {
       console.log('몽고디비 연결 에러', error);
